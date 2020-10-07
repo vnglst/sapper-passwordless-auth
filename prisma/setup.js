3f815f29fb158b915540b69806b20d30498f1e6f
Koen van Gilst <koen@koenvangilst.nl>
2020-10-07 11:11:58 +0200

    adds setup script

---
+++ prisma/setup.js
@@ -0,0 +1,41 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn"],
+});
+
+async function up() {
+  await prisma.$queryRaw`
+        CREATE TABLE "public"."User" (
+            id SERIAL PRIMARY KEY NOT NULL,
+            name VARCHAR(255),
+            email VARCHAR(255) UNIQUE NOT NULL
+        );
+    `;
+
+  await prisma.$queryRaw`
+        CREATE TABLE "public"."Profile" (
+            id SERIAL PRIMARY KEY NOT NULL,
+            bio TEXT,
+            "userId" INTEGER UNIQUE NOT NULL,
+            FOREIGN KEY ("userId") REFERENCES "public"."User"(id)
+        );
+    `;
+
+  return prisma.$disconnect();
+}
+
+async function down() {
+  await prisma.$queryRaw`
+    DROP TABLE "public"."Profile";
+`;
+
+  await prisma.$queryRaw`
+        DROP TABLE "public"."User";
+    `;
+
+  return prisma.$disconnect();
+}
+
+up();
+// down();
